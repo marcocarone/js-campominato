8ba13032f72d8866319e24af020322d8d05eeae7
Marco <marco.carone@libero.it>
2020-01-14 19:30:53 +0100

    aggiornamento bonus

--- BONUS/js/script.js
+++ BONUS/js/script.js
@@ -5,27 +5,27 @@ var possibilita = 84;
 var numeroUtente;
 var trovato = false;
 var livello;
-var difficolta;
 var numeroMax;
 
-difficolta = prompt("Inserisci il livello di difficoltà: 0, 1 oppure 2");
+livello = parseInt(prompt("Inserisci il livello di difficoltà: 0, 1 oppure 2"));
+console.log(livello);
 
-switch (difficolta) {
-  case "0":
+switch (livello) {
+  case 0:
     numeroMax = 100;
     possibilita = 84;
     break;
-  case "1":
+  case 1:
     numeroMax = 80;
     possibilita = 64;
     break;
-  case "2":
+  case 2:
     numeroMax = 50;
     possibilita = 34;
     break;
   default:
-    while (difficolta > 2) {
-      difficolta = prompt("Per favore inserisci il livello corretto di difficoltà: 0, 1 oppure 2");
+    while (livello != 0 && livello != 1 && livello != 2 ) {
+    livello = parseInt(prompt("Per favore inserisci il livello corretto di difficoltà: 0, 1 oppure 2"));
     }
 }
 
@@ -45,13 +45,13 @@ document.getElementById("numeri-random").innerHTML = numeriRandom;
 // l'utente inserisce un numero per 84 tentativi
 var x = 0;
 while (x < possibilita && trovato == false) {
-  if (difficolta == "0") {
+  if (livello == 0) {
     numeroUtente = prompt('Inserisci un numero da 1 a 100')
     console.log('Numero inserito: ' + numeroUtente);
-  } else if (difficolta == "1") {
+  } else if (livello == 1) {
     numeroUtente = prompt('Inserisci un numero da 1 a 80')
     console.log('Numero inserito: ' + numeroUtente);
-  } else if (difficolta == "2") {
+  } else if (livello == 2) {
     numeroUtente = prompt('Inserisci un numero da 1 a 50')
     console.log('Numero inserito: ' + numeroUtente);
   }
